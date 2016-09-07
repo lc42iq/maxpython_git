06013dd229ae1e98c6650ec77a8af5f1bdb56b9c
lc42iq <lc@42iq.com>
2016-09-07 11:18:07 +0100

    reformat image class

--- pillow/image_resize_pillow.py
+++ pillow/image_resize_pillow.py
@@ -1,24 +1,34 @@
 from PIL import Image
-import glob, os
+import os
 
 
-def resize(dir,save_dir):
-    count = 0
-    for fn in os.listdir(str(dir)):
-        namefile = fn.split(".")
-        count +=1
-        if namefile[1] in('db',''):
-            continue
-        print fn
+class ImageTools(object):
+    def __init__(self, in_path, out_path, quality=75, im_format=".jpg", im_width=2048):
+        self.im_width = im_width
+        self.quality = quality
+        self.out_path = out_path
+        self.in_path = in_path
+        self.im_format = im_format
+
+    def resize(self):
+        count = 0
+        for fn in os.listdir(str(self.in_path)):
+            namefile = fn.split(".")
+            count += 1
+            print fn
+            try:
+                with Image.open(str(self.in_path) + "/" + str(fn)) as im:
+                    if im.width > self.im_width:
+                        x = im.width
+                        y = im.height
+                        cons = float(y) / x
+                        my_height = self.im_width * cons
+                        im = im.resize((self.im_width, int(my_height)), resample=0)
+                    im.save(str(self.out_path) + "/" + str(namefile[0]) + ".jpg", quality=self.quality)
+            except IOError:
+                pass
+
         print count
-        im = Image.open(str(dir) + "/" + str(fn))
-        if im.width > 2048:
-            x=im.width
-            y=im.height
-            cons= float(y)/x
-            myheight = 2048*cons
-            im = im.resize((2048, int(myheight)), resample=0)
 
-        im.save(str(save_dir)+ "/" + str(namefile[0]) + ".jpg", "JPEG")
 
-resize("D:/DOWNLOAD/textures2", "D:/DOWNLOAD/reduced2")
+ImageTools("D:/DOWNLOAD/textures2", "D:/DOWNLOAD/reduced2").resize()
